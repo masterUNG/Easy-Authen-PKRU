c37f7e178f50451896cb3850e39b0ab5142a10c7
MasterUNG <phrombutr@gmail.com>
2017-07-26 16:53:15 +0700

    Check User OK

--- Easy Authen/ViewController.swift
+++ Easy Authen/ViewController.swift
@@ -9,6 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Implicit
+    let strDicUser = ["master": "12345", "dora": "1234"]
+    
+    
+    
+    @IBOutlet weak var userTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBAction func loginButton(_ sender: Any) {
+        
+        //Get Value From TextField
+        let strUser: String = userTextField.text!
+        let strPassword: String = passwordTextField.text!
+        
+        print("User ==> \(strUser)")
+        print("Password ==> \(strPassword)")
+        
+        //Check User
+        if let strMyPassword = strDicUser[strUser] {
+            print("User OK")
+        }   else {
+            print("User False")
+        }
+        
+        
+        
+    }   // loginButton
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
